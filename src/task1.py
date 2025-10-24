66b8b350f3d83e852e8065e741d9925d2b1bfa1c
Andrei Makarov <aomak@yandex.ru>
2025-10-24 15:54:45 +0300

    готово

---
+++ src/task1.py
@@ -0,0 +1,15 @@
+salary = input("Введите свою зарплату: ")
+mortgage = input("Введите процент по ипотеке: ")
+life = input("Введите процент на жизнь: ")
+
+if not salary.isdigit() or not mortgage.isdigit() or not life.isdigit():
+    print("Не выпендривайтесь. Вводите числа!")
+elif int(mortgage) > 100:
+    print("Да вы совсем безбашенный")
+elif (int(mortgage) + int(life)) > 100:
+    print("А раньше головой нужно было думать, а не чем обычно")
+else:
+    print(f"На ипотеку было потрачено: {int(int(salary)*int(mortgage)/100*12)} рублей")
+    print(
+        f"Было накоплено: {int((int(salary)-int(salary)*int(mortgage)/100-int(salary)*int(life)/100)*12)} рублей"
+    )
