20e5619d63ca82b525d78754a19e423089e1ad8d
Andrei Makarov <aomak@yandex.ru>
2025-11-22 19:49:51 +0300

    палиндром

---
+++ src/lesson8_task1_hard.py
@@ -0,0 +1,30 @@
+
+
+
+# Задание 1 -- убираем любые символы из предложения
+
+import string
+
+while True:
+    symbols_for_delete = string.punctuation + ' '
+
+    string_comp = input("Введите любое предложение: \n")
+
+    if string_comp:
+        string_with_all_symbols = string_comp.lower()
+
+        symbols = []
+        for symbol in string_with_all_symbols:
+            if symbol not in symbols_for_delete:
+                symbols.append(symbol)
+        user_string = ''.join(symbols)
+        reverse_string = user_string[::-1]
+        if user_string == reverse_string:
+            print("Строка является палиндромом")
+        else:
+            print("Строка НЕ является палиндромом")
+        break
+    else:
+        question = input('Вы ничего не ввели. Хотите начать заново? Если да, введите любые символы, если нет - нажмите Enter: \n')
+        if not question:
+            break
