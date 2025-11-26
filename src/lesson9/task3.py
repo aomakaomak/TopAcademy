58da10d37ea2bb9e6db94d24284139288ff043a9
Andrei Makarov <aomak@yandex.ru>
2025-11-26 14:33:33 +0300

    задача 3

---
+++ src/lesson9/task3.py
@@ -0,0 +1,30 @@
+
+queries = [
+    'смотреть сериалы онлайн',
+    'новости спорта',
+    'афиша кино',
+    'курс доллара',
+    'сериалы этим летом',
+    'курс по питону',
+    'сериалы про спорт'
+]
+
+words_percent = {}
+
+length_of_queries = len(queries)
+
+for query in queries:
+    counter = 0
+    query_length = query.count(" ") + 1
+    if query_length in words_percent:
+        counter += 1
+        words_percent[query_length] += counter/length_of_queries*100
+    else:
+        counter = 1
+        words_percent[query_length] = counter/length_of_queries*100
+
+for index, value in words_percent.items():
+    print(f'Запросов из {index} слов -- {round(value)}%.')
+
+
+
