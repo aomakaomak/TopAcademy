45008d761716f4e8f5d4a4b95d93b2bb0fb67a89
Andrei Makarov <aomak@yandex.ru>
2025-12-03 18:05:40 +0300

    задание 2

--- src/lesson11/task1.py
+++ src/lesson11/task1.py
@@ -27,8 +27,22 @@ def get_ingredients_list(file_path: str) -> dict:
     return cook_book
 
 
-def get_shop_list_by_dishes(dishes, person_count):
-    return dishes
+def get_shop_list_by_dishes(dishes: list, person_count: int) -> dict:
+    """ Считаем чего и сколько нужно купить """
+    cook_book = get_ingredients_list(file_path)
+    shop_dict = {}
+    for dish in dishes:
+        for key, value in cook_book.items():
+            if key.lower() == dish.lower():
+                for ingredient in value:
+                    ingredient_value = {
+                        'measure': ingredient['measure'],
+                        'quantity': int(ingredient['quantity'])*person_count
+                    }
+                    if shop_dict.get(ingredient['ingredient_name']):
+                        ingredient_value['quantity'] += ingredient_value['quantity']
+                    shop_dict[ingredient['ingredient_name']] = ingredient_value
+    return shop_dict
 
 
 if __name__ == '__main__':
@@ -37,13 +51,27 @@ if __name__ == '__main__':
     cook_book_our = get_ingredients_list(file_path)
 
 
+    print('Задание 1')
     pprint.pprint(
         cook_book_our,
         width=120,
         sort_dicts=False,
         compact=True
     )
+    print('*'*20)
 
 
-    print(get_shop_list_by_dishes(cook_book_our, 5))
+    dish_list = ['омлет', 'фахитос']
+    person_count = 5
+    shop_list = get_shop_list_by_dishes(dish_list, person_count)
+
+    print(f'Задание 2. Считаем продукты для блюд {dish_list} на {person_count} персон.')
+
+    pprint.pprint(
+        shop_list,
+        width=120,
+        sort_dicts=False,
+        compact=True
+    )
+    print('*' * 20)
 
