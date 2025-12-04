4d60e8f2d09fd188366d888905ce8728be3f99ab
Andrei Makarov <aomak@yandex.ru>
2025-12-04 15:57:42 +0300

    задание 3

--- src/lesson11/task1-2.py
+++ src/lesson11/task1-2.py
@@ -1,9 +1,10 @@
 import pprint
 
-def get_ingredients_list(file_path: str) -> dict:
-    """ Из содержимого файла делаем словарь рецептов """
 
-    with open (file_path, 'r', encoding='UTF-8') as file:
+def get_ingredients_list(file_path: str) -> dict:
+    """Из содержимого файла делаем словарь рецептов"""
+
+    with open(file_path, "r", encoding="UTF-8") as file:
         data = file.readlines()
 
     cook_book = {}
@@ -13,11 +14,11 @@ def get_ingredients_list(file_path: str) -> dict:
         step = int(data[i + 1])
         ingr_list = []
         for n in range(i + 2, i + 2 + step):
-            row = data[n].strip().split(' | ')
+            row = data[n].strip().split(" | ")
             ingr_dict = {
-                'ingredient_name': row[0],
-                'quantity': row[1],
-                'measure': row[2],
+                "ingredient_name": row[0],
+                "quantity": row[1],
+                "measure": row[2],
             }
             ingr_list.append(ingr_dict)
 
@@ -28,7 +29,7 @@ def get_ingredients_list(file_path: str) -> dict:
 
 
 def get_shop_list_by_dishes(dishes: list, person_count: int) -> dict:
-    """ Считаем чего и сколько нужно купить """
+    """Считаем чего и сколько нужно купить"""
     cook_book = get_ingredients_list(file_path)
     shop_dict = {}
     for dish in dishes:
@@ -36,42 +37,29 @@ def get_shop_list_by_dishes(dishes: list, person_count: int) -> dict:
             if key.lower() == dish.lower():
                 for ingredient in value:
                     ingredient_value = {
-                        'measure': ingredient['measure'],
-                        'quantity': int(ingredient['quantity'])*person_count
+                        "measure": ingredient["measure"],
+                        "quantity": int(ingredient["quantity"]) * person_count,
                     }
-                    if shop_dict.get(ingredient['ingredient_name']):
-                        ingredient_value['quantity'] += ingredient_value['quantity']
-                    shop_dict[ingredient['ingredient_name']] = ingredient_value
+                    if shop_dict.get(ingredient["ingredient_name"]):
+                        ingredient_value["quantity"] += ingredient_value["quantity"]
+                    shop_dict[ingredient["ingredient_name"]] = ingredient_value
     return shop_dict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    file_path = 'src/lesson11/dishes.txt'
+    file_path = "src/lesson11/dishes.txt"
     cook_book_our = get_ingredients_list(file_path)
 
+    print("Задание 1")
+    pprint.pprint(cook_book_our, width=120, sort_dicts=False, compact=True)
+    print("*" * 20)
 
-    print('Задание 1')
-    pprint.pprint(
-        cook_book_our,
-        width=120,
-        sort_dicts=False,
-        compact=True
-    )
-    print('*'*20)
-
-
-    dish_list = ['омлет', 'фахитос']
+    dish_list = ["омлет", "фахитос"]
     person_count = 5
     shop_list = get_shop_list_by_dishes(dish_list, person_count)
 
-    print(f'Задание 2. Считаем продукты для блюд {dish_list} на {person_count} персон.')
+    print(f"Задание 2. Считаем продукты для блюд {dish_list} на {person_count} персон.")
 
-    pprint.pprint(
-        shop_list,
-        width=120,
-        sort_dicts=False,
-        compact=True
-    )
-    print('*' * 20)
-
+    pprint.pprint(shop_list, width=120, sort_dicts=False, compact=True)
+    print("*" * 20)
--- src/lesson11/task3.py
+++ src/lesson11/task3.py
@@ -1,37 +1,39 @@
-file1 = 'src/lesson11/files/file1.txt'
-file2 = 'src/lesson11/files/file2.txt'
-
-with open (file1, 'r', encoding='UTF-8') as file:
-    data1 = file.readlines()
-
-with open (file2, 'r', encoding='UTF-8') as file:
-    data2 = file.readlines()
-
-len_data1 = len(data1)
-len_data2 = len(data2)
-
-with open (file1, 'r', encoding='UTF-8') as file:
-    text1 = file.read()
-
-with open (file2, 'r', encoding='UTF-8') as file:
-    text2 = file.read()
-
-print(text1)
-print(text2)
+# Допустим, что у нас 100 файлов. Нужен универсальный механизм
+from pathlib import Path
 
 
-with open ('src/lesson11/files/file3.txt', 'w', encoding='UTF-8') as file:
-    if len_data2 < len_data1:
-        file.write(file2 + '\n')
-        file.write(str(len_data2) + '\n')
-        file.write(text2 + '\n')
-        file.write(file1 + '\n')
-        file.write(str(len_data1) + '\n')
-        file.write(text1 + '\n')
-    else:
-        file.write(file1 + '\n')
-        file.write(str(len_data1) + '\n')
-        file.write(text1 + '\n')
-        file.write(file2 + '\n')
-        file.write(str(len_data2) + '\n')
-        file.write(text2 + '\n')
+def get_filenames(path):
+    return [p.name for p in Path(path).iterdir() if p.is_file()]
+
+
+file_list = get_filenames("src/lesson11/files/")
+
+data_list = []
+len_list = []
+path_list = []
+
+
+for document in file_list:
+    with open(f"src/lesson11/files/{document}", "r", encoding="UTF-8") as file:
+        data = file.readlines()
+    data_list.append(data)
+    len_data = len(data)
+    len_list.append(len_data)
+    path_list.append(document)
+
+
+with open("src/lesson11/files/file_common.txt", "w", encoding="UTF-8") as file:
+    file.write("")
+
+
+min_number = 0
+while len_list:
+    min_number = min(len_list)
+    index_of_min_number = len_list.index(min_number)
+    len_list.pop(index_of_min_number)
+    min_data = data_list.pop(index_of_min_number)
+    min_file = path_list.pop(index_of_min_number)
+    with open("src/lesson11/files/file_common.txt", "a", encoding="UTF-8") as file:
+        file.write(min_file + "\n")
+        file.write(str(min_number) + "\n")
+        file.write("".join(min_data))
